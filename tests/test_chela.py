6d7ec700b102cb2b4563ba08cc1ef4cd2ef9d30b
Claudio <claudio.pereti@outlook.com>
2021-08-08 01:34:13 +0200

    test on from string to dict

--- tests/test_chela.py
+++ tests/test_chela.py
@@ -3,9 +3,22 @@ sys.path.append('chela/')
 import chela
 import pytest
 
-@pytest.mark.parametrize('formula',['','H'])
+@pytest.mark.parametrize('check_formula',
+                                   ['',
+                                   '1',
+                                   '123H',
+                                   '3O',
+                                   ])
 
 
-def tests_check_formula(formula):
+def test_check_formula(check_formula):
     with pytest.raises(ValueError):
-        chela.check_formula(formula)
+        chela.check_formula(check_formula)
+
+@pytest.mark.parametrize('string_formula,dict_formula',
+                                   [
+                                   ('H',{'H':1}),
+                                   ])
+
+def test_from_string_to_dict(string_formula,dict_formula):
+    assert chela.from_string_to_dict(string_formula) == dict_formula
