939c1b50e29f279214562a3b4ad7c643a900fb8b
Claudio <claudio.pereti@outlook.com>
2021-08-08 17:19:11 +0200

    commentedand added complete check formula

--- chela/chela.py
+++ chela/chela.py
@@ -1,13 +1,13 @@
 
 def basic_check_formula(formula):
     """Check the basic correctness of the chemical formula,i.e. empty string, absent element, presence of non alphanumeric value"""
-
+    #Check if the string is empty
     if not formula:
         raise EmptyFormula
-
+    #Check if the first element is present
     if formula[0].isnumeric():
         raise FirstElementAbsent
-
+    #Check the presence of non alphanumeric values, included -
     if not formula.isalnum():
         raise NonAlphaNumericValue
 
@@ -23,19 +23,41 @@ class NonAlphaNumericValue(ValueError):
 class NegativeQuantityElement(ValueError):
     pass
 
+class NonExistentElement(ValueError):
+    pass
+
 class ZeroQuantityElement(ValueError):
+    pass
+
+class RepeatedElement(ValueError):
     pass
 
 def advanced_check_formula(formula):
     """Check the advanced correctness fo the chemical formula, i.e. wrong chemical element, zero value, repeated element"""
 
     dict_formula = from_string_to_dict(formula)
-    dict_values = list(dict_formula.values())
+    chemical_element = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
 
-    for value in dict_values:
-
+    #Check the presence of a element with 0 quantity
+    for value in dict_formula.values():
         if value == 0:
             raise ZeroQuantityElement
+    #Check the presence of inexistent or mispelled element
+    for element in dict_formula.keys():
+        if not element in chemical_element:
+            raise NonExistentElement
+
+    #Check if an element is repeated in the formula, using the fact that a dictionary overwrite a key if alredy present
+    letters = ''.join(str(element) for element in dict_formula.keys())
+    numbers = ''.join(str(number) for number in dict_formula.values())
+    complete_formula = letters + numbers
+
+    if len(complete_formula) != len(formula):
+        raise RepeatedElement
+
+def check_formula(formula):
+    basic_check_formula(formula)
+    advanced_check_formula(formula)
 
 
 
